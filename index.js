220dc5e7c56126d22e347aaee9b5a16c4a1b05d5
손영준 <son-yeongjun@son-yeongjun-ui-MacBookPro.local>
2020-09-17 10:28:55 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.en.token;
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
